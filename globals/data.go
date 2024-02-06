fa080a1c2558fa8dafa8f7d9120dd51631d758ad
GadaLuBau <146705886+GadaLuBau1337@users.noreply.github.com>
2024-02-06 12:05:52 +0700

    Add files via upload

---
+++ globals/data.go
@@ -0,0 +1,9 @@
+package globals
+
+var (
+	// Table   = make(map[string][]string, 0)
+	UAS     = make([]string, 0)
+	ACCEPTS = make([]string, 0)
+	REFS    = make([]string, 0)
+	PROXIES = make([]string, 0)
+)
